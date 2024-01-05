5c7e405befb53d67743f8de4b57cde46e958c521
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-05 10:33:08 +0300

    Proje dosyası ekle.

---
+++ DosyaIslemOrnek15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DosyaIslemOrnek15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] veriler = File.ReadAllLines("kayitlar.txt",Encoding.Default);
+            foreach (var item in veriler)
+            {
+                String[] dizi = item.Split(';');
+                //split komutu ile veri ; karakterini kullanarak bölünür ve diziye dönüştürülür.
+                Double arasinav =Convert.ToDouble( dizi[2]);
+                Double final = Convert.ToDouble(dizi[3]);
+                Double ortalama = arasinav * 0.4 + final * 0.6;
+                Console.WriteLine(dizi[0] + " "+dizi[1] + " " + dizi[2] + " " + dizi[3] + " " + ortalama);
+            }
+        }
+    }
+}
